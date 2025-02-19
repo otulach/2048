f47730dd3c15ffef8c467f145af6d22bcadf1fde
Ondřej Tulach <ondrej.tulach@gmail.com>
2025-02-19 09:27:47 +0100

    Working condition, without numbers

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System.Data.Common;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System;
+using System.Drawing;
+
+namespace _2048
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Model modelHry = new Model();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Grid.Focus();
+            modelHry.Pridej(2);
+        }
+   
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public SolidColorBrush dostanBarvu(int value)
+        {
+            switch (value)
+            {
+                case 2: return new SolidColorBrush(Colors.LightGray);
+                case 4: return new SolidColorBrush(Colors.LightBlue);
+                case 8: return new SolidColorBrush(Colors.Orange);
+                case 16: return new SolidColorBrush(Colors.DarkOrange);
+                case 32: return new SolidColorBrush(Colors.Red);
+                case 64: return new SolidColorBrush(Colors.DarkRed);
+                case 128: return new SolidColorBrush(Colors.Yellow);
+                case 256: return new SolidColorBrush(Colors.Gold);
+                case 512: return new SolidColorBrush(Colors.LightGreen);
+                case 1024: return new SolidColorBrush(Colors.Green);
+                case 2048: return new SolidColorBrush(Colors.DarkGreen);
+                default: return new SolidColorBrush(Colors.Black); // Default color for empty tiles
+            }
+        }
+
+        private void Posunuj(object sender, KeyEventArgs e)
+        {
+            Key kliknuty = e.Key;
+
+            int vektorX = 0;
+            int vektorY = 0;
+            // Check if the key pressed is a specific key
+            if (kliknuty == Key.Up)
+            {
+                vektorX = 0;
+                vektorY = -1;
+            }
+            else if (kliknuty == Key.Down)
+            {
+                vektorX = 0;
+                vektorY = 1;
+            }
+            else if (kliknuty == Key.Left)
+            {
+                vektorX = -1;
+                vektorY = 0;
+            }
+            else if (kliknuty == Key.Right)
+            {
+                vektorX = 1;
+                vektorY = 0;
+            }
+
+            if (!(vektorX == 0 & vektorY == 0))
+            {
+                try
+                {
+                    modelHry.Posun(vektorY, vektorX);
+                    modelHry.Pridej(2);
+                }
+                catch
+                {
+                    MessageBox.Show("No more moves available. You reached score " + modelHry.skore.ToString(), "Game Over", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+
+            Skore.Content = modelHry.skore.ToString();
+
+            foreach (UIElement element in Grid.Children)
+            {
+                int radek = Grid.GetRow(element);
+                int sloupec = Grid.GetColumn(element);
+
+                int hodnotaBoxu = modelHry.pole[radek, sloupec];
+
+                if (element is System.Windows.Shapes.Rectangle rectangle)
+                {
+                    rectangle.Fill = hodnotaBoxu >= 2 ? dostanBarvu(hodnotaBoxu) : new SolidColorBrush(Colors.Beige);
+                }
+            }
+        }
+    }
+}
---
+++ Model.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2048
+{
+    class Model
+    {
+        public int[,] pole = { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+
+        public int skore = 0;
+
+        public void Pridej(int cislo)
+        {
+            List<Tuple<int,int>> prazdne = new List<Tuple<int, int>>();
+
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    if (pole[x, y] == 0)
+                    {
+                        prazdne.Add(new Tuple<int, int>(x, y));
+                    }
+                }
+            }
+
+            Random random = new Random();
+            int nahodne = random.Next(0, prazdne.Count);
+
+            Tuple<int, int> souradnice = prazdne[nahodne];
+            pole[souradnice.Item1, souradnice.Item2] = cislo;
+        }
+
+        public void Posun(int x, int y)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    int radek = (x == 1) ? 3 - i : i;
+                    int sloupec = (y == 1) ? 3 - j : j;
+
+                    PosunJeden(radek, sloupec, x, y);
+                }
+            }
+        }
+
+        public void PosunJeden(int realneX, int realneY, int x, int y)
+        {
+            if (pole[realneX, realneY] == 0) return;
+
+            int nextX = realneX + x;
+            int nextY = realneY + y;
+
+            while (Uvnitr(nextX, nextY))
+            {
+                if (pole[nextX, nextY] == 0)
+                {
+                    pole[nextX, nextY] = pole[realneX, realneY];
+                    pole[realneX, realneY] = 0;
+                    realneX = nextX;
+                    realneY = nextY;
+                    nextX += x;
+                    nextY += y;
+                }
+                else if (pole[nextX, nextY] == pole[realneX, realneY])
+                {
+                    // Spojování
+                    pole[nextX, nextY] *= 2;
+                    skore += pole[nextX, nextY];
+                    pole[realneX, realneY] = 0;
+                    break;
+                }
+                else
+                {
+                    break; // Nemůže dál
+                }
+            }
+        }
+
+        public bool Misto(int realneX, int realneY, int x, int y)
+        {
+            while (Uvnitr(realneX + x, realneY + y))
+            {
+                if (pole[realneX + x, realneY + y] == 0)
+                {
+                    return true; // Neni místo
+                }
+                realneX += x;
+                realneY += y;
+            }
+            return false;
+        }
+
+        public bool Uvnitr(int x, int y)
+        {
+            return x >= 0 && x < 4 && y >= 0 && y < 4;
+        }
+    }
+}
